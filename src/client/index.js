78a78f7a0338fe666c7587f5a2088266386394c9
Oracle Meta Technologies <130051349+OracleMetaTechnologies@users.noreply.github.com>
2023-04-22 11:49:08 +0300

    Update index.js

--- src/client/index.js
+++ src/client/index.js
@@ -447,3 +447,469 @@ export class BncClient {
       msgType: "NewOrderMsg",
     }
     
+const signMsg = {
+      id: placeOrderMsg.id,
+      ordertype: placeOrderMsg.ordertype,
+      price: placeOrderMsg.price,
+      quantity: placeOrderMsg.quantity,
+      sender: address,
+      side: placeOrderMsg.side,
+      symbol: placeOrderMsg.symbol,
+      timeinforce: timeinforce,
+    }
+
+    checkNumber(placeOrderMsg.price, "price")
+    checkNumber(placeOrderMsg.quantity, "quantity")
+
+    const signedTx = await this._prepareTransaction(placeOrderMsg, signMsg, address, sequence, "")
+    return this._broadcastDelegate(signedTx)
+  }
+
+  /**
+   * @param {String} address
+   * @param {Number} proposalId
+   * @param {String} baseAsset
+   * @param {String} quoteAsset
+   * @param {Number} initPrice
+   * @param {Number} sequence optional sequence
+   * @return {Promise} resolves with response (success or fail)
+   */
+  async list(address, proposalId, baseAsset, quoteAsset, initPrice, sequence = null) {
+    const accCode = crypto.decodeAddress(address)
+
+    if (!address) {
+      throw new Error("address should not be falsy")
+    }
+
+    if(proposalId <= 0){
+      throw new Error("proposal id should larger than 0")
+    }
+
+    if(initPrice <= 0){
+      throw new Error("price should larger than 0")
+    }
+
+    if (!baseAsset) {
+      throw new Error("baseAsset should not be falsy")
+    }
+
+    if (!quoteAsset) {
+      throw new Error("quoteAsset should not be falsy")
+    }
+
+    const init_price = Number(new Big(initPrice).mul(BASENUMBER).toString())
+
+    const listMsg = {
+      from: accCode,
+      proposal_id: proposalId,
+      base_asset_symbol: baseAsset,
+      quote_asset_symbol: quoteAsset,
+      init_price: init_price,
+      msgType: "ListMsg"
+    }
+
+    const signMsg = {
+      base_asset_symbol: baseAsset,
+      from: address,
+      init_price: init_price,
+      proposal_id: proposalId,
+      quote_asset_symbol: quoteAsset,
+    }
+
+    const signedTx = await this._prepareTransaction(listMsg, signMsg, address, sequence, "")
+    return this._broadcastDelegate(signedTx)
+  }
+
+  /**
+   * Set account flags
+   * @param {String} address
+   * @param {Number} flags new value of account flags
+   * @param {Number} sequence optional sequence
+   * @return {Promise} resolves with response (success or fail)
+   */
+  async setAccountFlags(address, flags, sequence = null) {
+    const accCode = crypto.decodeAddress(address)
+
+    const msg = {
+      from: accCode,
+      flags: flags,
+      msgType: "SetAccountFlagsMsg"
+    }
+
+    const signMsg = {
+      flags: flags,
+      from: address
+    }
+
+    const signedTx = await this._prepareTransaction(msg, signMsg, address, sequence, "")
+    return this._broadcastDelegate(signedTx)
+  }
+
+  /**
+   * Prepare a serialized raw transaction for sending to the blockchain.
+   * @param {Object} msg the msg object
+   * @param {Object} stdSignMsg the sign doc object used to generate a signature
+   * @param {String} address
+   * @param {Number} sequence optional sequence
+   * @param {String} memo optional memo
+   * @return {Transaction} signed transaction
+   */
+  async _prepareTransaction(msg, stdSignMsg, address, sequence = null, memo = "") {
+    if ((!this.account_number || (sequence !== 0 && !sequence)) && address) {
+      const data = await this._httpClient.request("get", `${api.getAccount}/${address}`)
+      sequence = data.result.sequence
+      this.account_number = data.result.account_number
+      // if user has not used `await` in its call to setPrivateKey (old API), we should wait for the promise here
+    } else if (this._setPkPromise) {
+      await this._setPkPromise
+    }
+
+    const options = {
+      account_number: parseInt(this.account_number),
+      chain_id: this.chainId,
+      memo: memo,
+      msg,
+      sequence: parseInt(sequence),
+      source: this._source,
+      type: msg.msgType,
+    }
+
+    const tx = new Transaction(options)
+    return this._signingDelegate.call(this, tx, stdSignMsg)
+  }
+
+  /**
+   * Broadcast a transaction to the blockchain.
+   * @param {signedTx} tx signed Transaction object
+   * @param {Boolean} sync use synchronous mode, optional
+   * @return {Promise} resolves with response (success or fail)
+   */
+  async sendTransaction(signedTx, sync) {
+    const signedBz = signedTx.serialize()
+    return this.sendRawTransaction(signedBz, sync)
+  }
+
+  /**
+   * Broadcast a raw transaction to the blockchain.
+   * @param {String} signedBz signed and serialized raw transaction
+   * @param {Boolean} sync use synchronous mode, optional
+   * @return {Promise} resolves with response (success or fail)
+   */
+  async sendRawTransaction(signedBz, sync = !this._useAsyncBroadcast) {
+    const opts = {
+      data: signedBz,
+      headers: {
+        "content-type": "text/plain",
+      }
+    }
+    return this._httpClient.request("post", `${api.broadcast}?sync=${sync}`, null, opts)
+  }
+
+  /**
+   * Broadcast a raw transaction to the blockchain.
+   * @param {Object} msg the msg object
+   * @param {Object} stdSignMsg the sign doc object used to generate a signature
+   * @param {String} address
+   * @param {Number} sequence optional sequence
+   * @param {String} memo optional memo
+   * @param {Boolean} sync use synchronous mode, optional
+   * @return {Promise} resolves with response (success or fail)
+   */
+  async _sendTransaction(msg, stdSignMsg, address, sequence = null, memo = "", sync = !this._useAsyncBroadcast) {
+    const signedTx = await this._prepareTransaction(msg, stdSignMsg, address, sequence, memo)
+    return this.sendTransaction(signedTx, sync)
+  }
+
+  /**
+   * get account
+   * @param {String} address
+   * @return {Promise} resolves with http response
+   */
+  async getAccount(address = this.address) {
+    if (!address) {
+      throw new Error("address should not be falsy")
+    }
+    try {
+      const data = await this._httpClient.request("get", `${api.getAccount}/${address}`)
+      return data
+    } catch (err) {
+      return null
+    }
+  }
+
+  /**
+   * get balances
+   * @param {String} address optional address
+   * @return {Promise} resolves with http response
+   */
+  async getBalance(address = this.address) {
+    try {
+      const data = await this.getAccount(address)
+      return data.result.balances
+    } catch (err) {
+      return []
+    }
+  }
+
+  /**
+   * get markets
+   * @param {Number} limit max 1000 is default
+   * @param {Number} offset from beggining, default 0
+   * @return {Promise} resolves with http response
+   */
+  async getMarkets(limit = 1000, offset = 0) {
+    try {
+      const data = await this._httpClient.request("get", `${api.getMarkets}?limit=${limit}&offset=${offset}`)
+      return data
+    } catch (err) {
+      console.warn("getMarkets error", err)
+      return []
+    }
+  }
+
+  /**
+   * get transactions for an account
+   * @param {String} address optional address
+   * @param {Number} offset from beggining, default 0
+   * @return {Promise} resolves with http response
+   */
+  async getTransactions(address = this.address, offset = 0) {
+    try {
+      const data = await this._httpClient.request("get", `${api.getTransactions}?address=${address}&offset=${offset}`)
+      return data
+    } catch (err) {
+      console.warn("getTransactions error", err)
+      return []
+    }
+  }
+
+  /**
+   * get transaction
+   * @param {String} hash the transaction hash
+   * @return {Promise} resolves with http response
+   */
+  async getTx(hash) {
+    try {
+      const data = await this._httpClient.request("get", `${api.getTx}/${hash}`)
+      return data
+    } catch (err) {
+      console.warn("getTx error", err)
+      return []
+    }
+  }
+
+  /**
+   * get depth for a given market
+   * @param {String} symbol the market pair
+   * @return {Promise} resolves with http response
+   */
+  async getDepth(symbol = "BNB_BUSD-BD1") {
+    try {
+      const data = await this._httpClient.request("get", `${api.getDepth}?symbol=${symbol}`)
+      return data
+    } catch (err) {
+      console.warn("getDepth error", err)
+      return []
+    }
+  }
+
+  /**
+   * get open orders for an address
+   * @param {String} address binance address
+   * @param {String} symbol binance BEP2 symbol
+   * @return {Promise} resolves with http response
+   */
+  async getOpenOrders(address = this.address) {
+    try {
+      const data = await this._httpClient.request("get", `${api.getOpenOrders}?address=${address}`)
+      return data
+    } catch (err) {
+      console.warn("getOpenOrders error", err)
+      return []
+    }
+  }
+
+  /**
+   * get atomic swap
+   * @param {String} swapID: ID of an existing swap
+   * @return {Promise} AtomicSwap
+   */
+  async getSwapByID(swapID) {
+    try {
+      const data = await this._httpClient.request("get", `${api.getSwaps}/${swapID}`)
+      return data
+    } catch (err) {
+      console.warn("query swap by swapID error", err)
+      return []
+    }
+  }
+
+  /**
+   * query atomic swap list by creator address
+   * @param {String} creator: swap creator address
+   * @param {Number} offset from beginning, default 0
+   * @param {Number} limit, max 1000 is default
+   * @return {Promise} Array of AtomicSwap
+   */
+  async getSwapByCreator(creator, limit = 100, offset = 0) {
+    try {
+      const data = await this._httpClient.request("get", `${api.getSwaps}?fromAddress=${creator}&limit=${limit}&offset=${offset}`)
+      return data
+    } catch (err) {
+      console.warn("query swap list by swap creator error", err)
+      return []
+    }
+  }
+
+  /**
+   * query atomic swap list by recipient address
+   * @param {String} recipient: the recipient address of the swap
+   * @param {Number} offset from beginning, default 0
+   * @param {Number} limit, max 1000 is default
+   * @return {Promise} Array of AtomicSwap
+   */
+  async getSwapByRecipient(recipient, limit = 100, offset = 0) {
+    try {
+      const data = await this._httpClient.request("get", `${api.getSwaps}?toAddress=${recipient}&limit=${limit}&offset=${offset}`)
+      return data
+    } catch (err) {
+      console.warn("query swap list by swap recipient error", err)
+      return []
+    }
+  }
+
+  /**
+   * Creates a private key and returns it and its address.
+   * @return {object} the private key and address in an object.
+   * {
+   *  address,
+   *  privateKey
+   * }
+   */
+  createAccount() {
+    const privateKey = crypto.generatePrivateKey()
+    return {
+      privateKey,
+      address: crypto.getAddressFromPrivateKey(privateKey, this.addressPrefix)
+    }
+  }
+
+  /**
+   * Creates an account keystore object, and returns the private key and address.
+   * @param {String} password
+   *  {
+   *  privateKey,
+   *  address,
+   *  keystore
+   * }
+   */
+  createAccountWithKeystore(password) {
+    if (!password) {
+      throw new Error("password should not be falsy")
+    }
+    const privateKey = crypto.generatePrivateKey()
+    const address = crypto.getAddressFromPrivateKey(privateKey, this.addressPrefix)
+    const keystore = crypto.generateKeyStore(privateKey, password)
+    return {
+      privateKey,
+      address,
+      keystore
+    }
+  }
+
+  /**
+   * Creates an account from mnemonic seed phrase.
+   * @return {object}
+   * {
+   *  privateKey,
+   *  address,
+   *  mnemonic
+   * }
+   */
+  createAccountWithMneomnic() {
+    const mnemonic = crypto.generateMnemonic()
+    const privateKey = crypto.getPrivateKeyFromMnemonic(mnemonic)
+    const address = crypto.getAddressFromPrivateKey(privateKey, this.addressPrefix)
+    return {
+      privateKey,
+      address,
+      mnemonic
+    }
+  }
+
+  /**
+   * Recovers an account from a keystore object.
+   * @param {object} keystore object.
+   * @param {string} password password.
+   * {
+   * privateKey,
+   * address
+   * }
+   */
+  recoverAccountFromKeystore(keystore, password) {
+    const privateKey = crypto.getPrivateKeyFromKeyStore(keystore, password)
+    const address = crypto.getAddressFromPrivateKey(privateKey, this.addressPrefix)
+    return {
+      privateKey,
+      address
+    }
+  }
+
+  /**
+   * Recovers an account from a mnemonic seed phrase.
+   * @param {string} mneomnic
+   * {
+   * privateKey,
+   * address
+   * }
+   */
+  recoverAccountFromMnemonic(mnemonic) {
+    const privateKey = crypto.getPrivateKeyFromMnemonic(mnemonic)
+    const address = crypto.getAddressFromPrivateKey(privateKey, this.addressPrefix)
+    return {
+      privateKey,
+      address
+    }
+  }
+  // support an old method name containing a typo
+  recoverAccountFromMneomnic(mnemonic) {
+    return this.recoverAccountFromMnemonic(mnemonic)
+  }
+
+  /**
+   * Recovers an account using private key.
+   * @param {String} privateKey
+   * {
+   * privateKey,
+   * address
+   * }
+   */
+  recoverAccountFromPrivateKey(privateKey) {
+    const address = crypto.getAddressFromPrivateKey(privateKey, this.addressPrefix)
+    return {
+      privateKey,
+      address
+    }
+  }
+
+  /**
+   * Validates an address.
+   * @param {String} address
+   * @param {String} prefix
+   * @return {Boolean}
+   */
+  checkAddress(address, prefix = this.addressPrefix) {
+    return crypto.checkAddress(address, prefix)
+  }
+
+  /**
+   * Returns the address for the current account if setPrivateKey has been called on this client.
+   * @return {String}
+   */
+  getClientKeyAddress() {
+    if (!this.privateKey) throw new Error("no private key is set on this client")
+    const address = crypto.getAddressFromPrivateKey(this.privateKey, this.addressPrefix)
+    this.address = address
+    return address
+  }
+}
