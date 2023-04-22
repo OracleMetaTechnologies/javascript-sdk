91fa73ad5dadc3d23a1045631caca4e3246b2f75
Oracle Meta Technologies <130051349+OracleMetaTechnologies@users.noreply.github.com>
2023-04-22 11:40:45 +0300

    Update index.js

--- src/client/index.js
+++ src/client/index.js
@@ -11,3 +11,20 @@ import Gov from "../gov/"
 import Big from "big.js"
 
 const BASENUMBER = Math.pow(10, 8)
+
+export const api = {
+  broadcast: "/api/v1/broadcast",
+  nodeInfo: "/api/v1/node-info",
+  getAccount: "/api/v1/account",
+  getMarkets: "/api/v1/markets",
+  getSwaps: "/api/v1/atomic-swaps",
+  getOpenOrders: "/api/v1/orders/open",
+  getDepth: "/api/v1/depth",
+  getTransactions: "/api/v1/transactions",
+  getTx: "/api/v1/tx"
+}
+
+const NETWORK_PREFIX_MAPPING = {
+  "testnet": "tbnb",
+  "mainnet": "bnb"
+}
