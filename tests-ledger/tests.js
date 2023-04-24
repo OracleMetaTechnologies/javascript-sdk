59d8c90ebc1e6c42f5ade244cb4400a015393a3a
Oracle Meta Technologies <130051349+OracleMetaTechnologies@users.noreply.github.com>
2023-04-24 15:10:58 +0300

    Update tests.js

--- tests-ledger/tests.js
+++ tests-ledger/tests.js
@@ -22,3 +22,32 @@ if (typeof window !== "undefined") {
   Ledger = require("../lib/ledger")
   crypto = require("../lib/crypto")
 }
+
+//#region Init Connection
+
+const getApp = async function(timeout = LONG_TIMEOUT) {
+  const transClass = isBrowser ? Ledger.transports.u2f : Ledger.transports.node
+  const transport = await transClass.create(timeout)
+  app = new Ledger.app(transport)
+
+  return app
+}
+
+QUnit.begin(async function() {
+  console.log(
+    "Attempting to connect to hardware wallet, please ensure that it is plugged in and the app is open."
+  )
+  console.log(
+    "If the device is not connected or the app is not open, exit and rerun this test when it is."
+  )
+  try {
+    app = await getApp()
+  } catch (err) {
+    console.error(
+      "Unable to connect to hardware wallet. Please connect it and open the app.",
+      err
+    )
+  }
+})
+
+//#endregion
